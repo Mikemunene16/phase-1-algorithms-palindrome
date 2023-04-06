a6ea25bf380c8798f17fa6b8080eba88d7c934c5
Mike <mike.munene@student.moringaschool.com>
2023-04-06 16:55:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // const reverse = word.split("").reverse().join("");
+  return word === word.split("").reverse().join("") ? true : false;
 }
 
 /* 
   Add your pseudocode here
+  *1. Split the string 2.reverse the string,  3. Join the string, 4 write a condition that checks whether the word is the same when reversed.
 */
 
 /*
   Add written explanation of your solution here
+  ? Check whether if a word will read the same when the word is reversed
 */
 
 // You can run `node index.js` to view these console logs
